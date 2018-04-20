49a1ab0567c08e1f15496439af7904c982dbf906
Stevanato Giovanni <giovanni.stevanato>
2018-04-20 09:56:05 +0200

    aggiornato codice fiscale material design

--- codice fiscale md/js/init.js
+++ codice fiscale md/js/init.js
@@ -1,10 +1,15 @@
-(function($){
+(function ($) {
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-	$(document).ready(function(){
+	$('.sidenav').sidenav();
+	$('.parallax').parallax();
+	$(document).ready(function () {
+		
 		$('select').formSelect();
-		$('.datepicker').datepicker();
+		$('.datepicker').datepicker({
+			yearRange: 100,
+			format: "yyyy-mm-dd"
+		});
+
 	});
-	
+
 })(jQuery);
